4f5843a4cfe93658543f278a2e31f37427de8a00
Fabio Codiglioni <fabiocody@icloud.com>
2019-09-04 14:29:09 +0200

    Initial Commit

---
+++ EasyRepay/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  EasyRepay
+//
+//  Created by Fabio Codiglioni on 04/09/2019.
+//  Copyright © 2019 Fabio Codiglioni. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
